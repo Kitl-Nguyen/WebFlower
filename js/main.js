520df0915bf0749d02b58db033367350fea5ea68
Yeun <tkng2001@gmail.com>
2024-11-03 23:16:57 +0700

    Fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+    $('#mainSlider').nivoSlider({
+		directionNav: true,
+		animSpeed: 500,
+		slices: 18,
+		pauseTime: 5000,
+		pauseOnHover: false,
+		controlNav: false,
+		prevText: '<i class="fa fa-angle-left nivo-prev-icon"></i>',
+		nextText: '<i class="fa fa-angle-right nivo-next-icon"></i>'
+	});
 	$(".owl-carousel").owlCarousel({
 		loop: true,
 		margin: 10,
